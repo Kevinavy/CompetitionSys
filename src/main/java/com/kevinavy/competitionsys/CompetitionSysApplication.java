65c9e74a5565838686d05f30dc7ec9da64b8335a
Kevinavy <724072210@qq.com>
2022-04-25 22:58:50 +0800

    Initial commit

---
+++ src/main/java/com/kevinavy/competitionsys/CompetitionSysApplication.java
@@ -0,0 +1,13 @@
+package com.kevinavy.competitionsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionSysApplication.class, args);
+    }
+
+}
